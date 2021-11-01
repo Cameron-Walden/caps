b7106a18236bae8f80858ad035d504c144516d49
Cameron <cameronkwalden@gmail.com>
2021-11-01 16:56:13 -0700

    imports faker to mock customer orders

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -2,14 +2,22 @@
 
 const driver = require('../driver/driver.js');
 const vendorPackage = require('../events/events.js');
+const faker = require('faker');
 
 // 1. As a vendor, I want to alert the system when I have a package to be picked up.
 const input = process.argv[2];
-const eventId = Math.floor(Math.random() * 1000000);
+const orderId = Math.floor(Math.random() * 1000000);
 
 vendorPackage.on('events',(payload) => {
   console.log('Message receieved by: ', payload)
 });
+
+// vendorPackage.on('order', {
+//   store: faker.company.companyName(),
+//   orderId: faker.datatype.uuid(),
+//   customer: faker.name.findName(),
+//   address: faker.address.streetAddress(),
+// });
 
 // 4. as a driver, I want to alert the system when a vendorPackage has been delivered.
 // 5. As a vendor, I want to be notified when my vendorPackage has been delivered.
@@ -18,4 +26,3 @@ vendorPackage.on('events',(payload) => {
 vendorPackage.on('events', (payload) => {
   console.log('Thank you for the delivery!', payload);
 });
-
