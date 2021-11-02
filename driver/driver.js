a37e7fa10dec23d08d20b6871c9479af127fea0c
Cameron <cameronkwalden@gmail.com>
2021-11-01 20:27:27 -0700

    cleans code up. vendor/driver communicating

--- driver/driver.js
+++ driver/driver.js
@@ -4,28 +4,36 @@ const vendor = require('../vendor/vendor.js');
 const vendorPackage = require('../events/events.js');
 
 const input = process.argv[2];
-const orderId = Math.floor(Math.random() * 1000000);
 const faker = require('faker');
 
-// 2. As a driver, I want to be notified when there is a package to be delivered.
-vendorPackage.on('events',(payload) => {
-    let clientId = Math.floor(Math.random() * 1000000);
-    // 3. As a driver, I want to alert the system when I have picked up a vendorPackage and it is in transit
-  console.log('Driver was alerted and is on their way!', payload);
+    vendorPackage.on('pickup', (payload) => {
+    
+      console.log(`Your package has been has been picked up and is now out for delivery.`, payload)
+    
+      vendorPackage.emit('in-transit', {
+        event: 'in-transit',
+        time: faker.date.soon(),
+        payload: {
+          store: faker.company.companyName(),
+          orderId: faker.datatype.uuid(),
+          customer: faker.name.findName(),
+          address: faker.address.streetAddress(),
+        }
+      });
+    });
 
+    vendorPackage.on('in-transit', (payload) => {
 
-  // vendorPackage.emit('received', {clientId});
-  vendorPackage.emit('received', {clientId});
-});
-
-// vendorPackage.emit('events', {
-//   text: input,
-//   id: orderId,
-// });
-
-vendorPackage.emit('events', {
-  store: faker.company.companyName(),
-  orderId: faker.datatype.uuid(),
-  customer: faker.name.findName(),
-  address: faker.address.streetAddress(),
-});
+      console.log(`Your driver ${faker.name.findName()} has delivered the order!`, payload)
+    
+      vendorPackage.emit('delivered', {
+        event: 'delivered',
+        time: faker.date.soon(),
+        payload: {
+          store: faker.company.companyName(),
+          orderId: faker.datatype.uuid(),
+          customer: faker.name.findName(),
+          address: faker.address.streetAddress(),
+        }
+      });
+    });
--- vendor/vendor.js
+++ vendor/vendor.js
@@ -4,25 +4,29 @@ const driver = require('../driver/driver.js');
 const vendorPackage = require('../events/events.js');
 const faker = require('faker');
 
-// 1. As a vendor, I want to alert the system when I have a package to be picked up.
 const input = process.argv[2];
-const orderId = Math.floor(Math.random() * 1000000);
 
-vendorPackage.on('events',(payload) => {
-  console.log('Message receieved by: ', payload)
+vendorPackage.on('delivered', (payload) => {
+  console.log('Thank you for the safe delivery!', payload);
 });
 
-// vendorPackage.on('order', {
-//   store: faker.company.companyName(),
-//   orderId: faker.datatype.uuid(),
-//   customer: faker.name.findName(),
-//   address: faker.address.streetAddress(),
-// });
+vendorPackage.on('order', () => {
 
-// 4. as a driver, I want to alert the system when a vendorPackage has been delivered.
-// 5. As a vendor, I want to be notified when my vendorPackage has been delivered.
-//this is the mesg the vndr sends to driver when pack is delvrd
+  vendorPackage.emit('pickup', {
+    event: 'pickup',
+    time: faker.date.soon(),
+    payload: {
+      store: faker.company.companyName(),
+      orderId: faker.datatype.uuid(),
+      customer: faker.name.findName(),
+      address: faker.address.streetAddress(),
+    }
+  });
+});
 
-vendorPackage.on('events', (payload) => {
-  console.log('Thank you for the delivery!', payload);
+vendorPackage.emit('order', {
+  store: faker.company.companyName(),
+  orderId: faker.datatype.uuid(),
+  customer: faker.name.findName(),
+  address: faker.address.streetAddress(),
 });
